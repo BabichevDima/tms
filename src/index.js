16ec3ee484dc78481cff3143f5f38efc01b287e6
Arseniy Shmelev <7080446@gmail.com>
2020-09-29 22:33:39 +0300

    updated feature section

--- src/index.js
+++ src/index.js
@@ -11,3 +11,26 @@ button.addEventListener("click", () => {
 	button.classList.add("active")
 	menu.classList.add("active")
 })
+
+const clear = (tabsItems, list) => {
+	tabsItems.map((tab, index) => {
+		tab.classList.remove("features__button_active")
+		list[index].classList.remove("features__item_visible")
+	})
+}
+
+const tabs = () => {
+	const tabs = document.getElementById("tabs").children
+	const list = document.getElementById("list").children
+	const tabsItems = Array.prototype.slice.call(tabs, 0)
+
+	tabsItems.map((tab, index) => {
+		tab.addEventListener("click", function () {
+			clear(tabsItems, list)
+			this.classList.add("features__button_active")
+			list[index].classList.add("features__item_visible")
+		})
+	})
+}
+
+tabs()
