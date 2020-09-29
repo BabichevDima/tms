0148839ea07f9ba67960f5f75d022fcb64b2616d
Arseniy Shmelev <7080446@gmail.com>
2020-09-29 19:46:01 +0300

    added source map

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,31 @@ const path = require("path");
 const miniCss = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(s*)css$/,
-        use: [miniCss.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new miniCss({
-      filename: "style.css",
-    }),
-  ],
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.(s*)css$/,
+				use: [miniCss.loader, "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: "file-loader",
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new miniCss({
+			filename: "style.css",
+		}),
+	],
 };
